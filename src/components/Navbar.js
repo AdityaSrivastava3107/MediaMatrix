36cba0e5fd4b6aaea33818d18bbde4a58e41c80c
Aditya Srivastava <kallakallz123@gmail.com>
2023-04-06 17:17:49 +0530

    Added more pages to MediaMatrix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
-
 export class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container-fluid">
                 <a className="navbar-brand" href="/">MediaMatrix</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,10 +12,10 @@ export class Navbar extends Component {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="/">Home</a>
+                    <a className="nav-link active" aria-current="page" href="/">Headlines</a>
                     </li>
                     <li className="nav-item">
-                    <a className="nav-link" href="/">About</a>
+                    <a className="nav-link" href="/">Sports</a>
                     </li>
                 </ul>
                 </div>
--- src/components/News.js
+++ src/components/News.js
@@ -1,30 +1,68 @@
 import React, { Component } from 'react'
 import NewsList from './NewsItem'
+import Spinner from './Spinner';
 
 export class News extends Component {
   constructor(){
     super();
     this.state = {
       articles: [],
-      loading: false
+      loading: false,
+      page: 1
     }
   }
   async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=02e342880cbe4c5b8b4705fa883b45d3";
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=02e342880cbe4c5b8b4705fa883b45d3&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true});
     let data = await fetch(url);
     let parsedData = await data.json()
-    this.setState({articles: parsedData.articles})
+    this.setState({articles: parsedData.articles,
+       totalResults: parsedData.totalResults,
+       loading: false})
   }
+
+  handlepreviousClick = async ()=>{
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=02e342880cbe4c5b8b4705fa883b45d3&page=${this.state.page-1}&pagesize=${this.props.pageSize}`;
+    this.setState({loading:true});
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    this.setState({
+      page: this.state.page -1,
+      articles: parsedData.articles,
+      loading: false
+    })
+  }
+  
+  handlenextClick = async ()=>{
+    if(this.state.page+1>Math.ceil(this.state.totalResults/this.props.pageSize)){
+    }
+    else{
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=02e342880cbe4c5b8b4705fa883b45d3&page=${this.state.page+1}&pagesize=${this.props.pageSize}`;
+      this.setState({loading:true});
+      let data = await fetch(url);
+      let parsedData = await data.json()
+      this.setState({
+        page: this.state.page +1,
+        articles: parsedData.articles,
+        loading: false
+      })}
+  }
+
   render() {
     return (
       <div className=" container my-4 ">
-        <h2><strong>MediaMatrix</strong>- Top Headlines</h2>
+        <h2 className="text-center"><strong>MediaMatrix</strong>- Headline Havoc</h2>
+        {this.state.loading && <Spinner/>}
         <div className="row">
-        {this.state.articles.map((element)=>{
+        {!this.state.loading &&this.state.articles.map((element)=>{
           return <div className="col-md-4" key={(element.url)}>
-          <NewsList title = {element.title?element.title.slice(0,50):""} description = {element.description?element.description.slice(0,59):""} imgurl = {element.urlToImage} Link = {element.url}/>
+          <NewsList title = {element.title} description = {element.description} imgurl = {element.urlToImage} Link = {element.url}/>
           </div>
         })}
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page<=1}type="button" className="btn btn-dark" onClick={this.handlepreviousClick}>&larr; Previous</button>
+        <button disabled= {this.state.page+1>Math.ceil(this.state.totalResults/this.props.pageSize)}type="button" className="btn btn-dark" onClick={this.handlenextClick}>Next &rarr;</button>
         </div>
       </div>
     )
